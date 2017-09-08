a26df5e896037e56fe6cb5337af5f76b9d6ad71a
Colin McKean <ezikill@hotmail.com>
2017-09-08 11:01:08 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 def oxford_comma(array)
   thingy = array.join(",")
   thingy.split(",")
-  array[0..-2].join(", ") << ", and #{array.last}"
+  array[0..-1].join(", ") << ", and #{array.last}"
 
 end
