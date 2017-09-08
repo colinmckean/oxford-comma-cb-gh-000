ad067e011a0566fe7c670f50ed806c9fb153e85a
Colin McKean <ezikill@hotmail.com>
2017-09-08 14:48:38 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,6 @@
 def oxford_comma(array)
+  if array.size > 2
   array[0...-1].join(", ") << ", and #{array.last}"
-  # thingy.split(",")
-  # array[0..2].join(", ") << ", and #{array.last}"
+else
+  array.join(" and ")
 end
